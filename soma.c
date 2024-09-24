941d1a7b4e3b2de96e4ec08e42edfccfc8d76820
Gege <geovanna.pbs@gmail.com>
2024-09-24 11:15:11 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int numero1 = 6;
+int numero2 = 20;
+int soma;
+char letra = '1';
+char nome[8]={"geovanna"};
+printf("%c \n",letra);
+soma = numero1 + numero2;
+printf("%d",soma);
+}
