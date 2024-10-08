2666f0118ef58fdecb0b80a9443dec40a6f3e08d
Gege <geovanna.pbs@gmail.com>
2024-10-08 09:41:04 -0300

    Add files via upload

---
+++ if.else.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int Geovanna = 16;
+    int Felipe = 17;
+    
+    if(Felipe > Geovanna){
+        printf("Felipe e mais velho que Geovanna!");}
+
+        else printf("Geovanna e mais velha que Felipe!");}
